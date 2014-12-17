a6ced81ba08a5d0c767d0463a1a79bd5dc85b96d
Morten Henriksen <mh@gi2.dk>
2014-12-17 15:46:28 +0100

    bump versions

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   git: 'https://github.com/raix/Meteor-formular.git',
   name: 'raix:formular',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Adds Formular'
 });
 
 Package.onUse(function(api) {
-  
+
   api.versionsFrom && api.versionsFrom('1.0');
 
   api.use(['jquery'], 'client');
